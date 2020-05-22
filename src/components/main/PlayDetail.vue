0b792a6f576667990880bcd26d85918fd03d87bd
lxhyl <zhangpengfan6@gmail.com>
2020-05-22 11:01:00 +0800

    回huigun

--- src/components/main/PlayDetail.vue
+++ src/components/main/PlayDetail.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>播放详情页{{musicid}}</div>
+  <div v-if="getDataOk">
+    <div class="left">
+      <img :src="musicInfo.songs[0].al.picUrl" />
+    </div>
+    <div class="main-danmu">
+    </div>
+    <div class="lyric">
+
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,7 +26,8 @@ export default {
   data() {
     return {
       musicid: null,
-      musicInfo:null,
+      musicInfo: null,
+      getDataOk: false //是否拿到数据
     };
   },
   created() {
@@ -31,10 +41,11 @@ export default {
       this.$axios
         .get(`${this.$domain}/song/detail?ids=${this.musicid}`)
         .then(res => {
-         //更新VUEX的音乐信息
-          this.$store.commit('getMusicInfo',res.data);
+          //更新VUEX的音乐信息
+          this.$store.commit("getMusicInfo", res.data);
           this.musicInfo = res.data;
-          // console.log(this.musicInfo);
+          this.getDataOk = true;
+          console.log(this.musicInfo);
         });
     }
   }
@@ -42,4 +53,46 @@ export default {
 </script>
 
 <style scoped>
+.left {
+  width: 300px;
+  height: 300px;
+  text-align: center;
+  float: left;
+}
+.left > img {
+  position: relative;
+  top: 50px;
+  height: 200px;
+  width: 200px;
+  border-radius: 100%;
+  animation: turn 16s linear infinite;
+}
+@keyframes turn {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  25% {
+    -webkit-transform: rotate(90deg);
+  }
+  50% {
+    -webkit-transform: rotate(180deg);
+  }
+  75% {
+    -webkit-transform: rotate(270deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+.main-danmu{
+  margin-left: 300px;
+  width: 520px;
+  height: 300px;
+  background-color: yellow;
+}
+.lyric{
+  width: 520px;
+  height: 268px;
+  background-color: violet;
+}
 </style>
