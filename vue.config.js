0dfce95f239948a11a61c761274ea72ee6ef2c50
lxhyl <zhangpengfan6@gmail.com>
2020-05-26 10:42:10 +0800

    add 骨架屏 gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -37,7 +37,7 @@ module.exports = {
                     new CompressionPlugin({
                         test: /\.js$|\.html$|.\css/, //匹配文件名
                         threshold: 10240,//对超过10k的数据压缩
-                        deleteOriginalAssets: false //不删除源文件
+                        deleteOriginalAssets: true //不删除源文件
                     })
                 ]
             }
